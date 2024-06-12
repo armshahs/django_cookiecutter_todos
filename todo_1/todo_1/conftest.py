f9f1122b6581f5ba253c4f56a6cba4e988454fca
admin <ar.mshahs@gmail.com>
2024-06-12 15:35:46 +0530

    0.0

---
+++ todo_1/todo_1/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from todo_1.users.models import User
+from todo_1.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
